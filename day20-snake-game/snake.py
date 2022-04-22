e7acda23832ccf6adfe67c8dca75d0f162fbbadf
eric <eric.arakaki@gmail.com>
2022-04-21 20:57:14 -1000

    completed snake game

--- day20-snake-game/snake.py
+++ day20-snake-game/snake.py
@@ -13,13 +13,20 @@ class Snake:
         self.create_snake()
         self.head = self.segments[0]
 
+
     def create_snake(self):
         for position in STARTING_POSITIONS:
-            new_segment = Turtle(shape="square")
-            new_segment.color("white")
-            new_segment.penup()
-            new_segment.goto(position)
-            self.segments.append(new_segment)
+            self.add_segment(position)
+
+    def add_segment(self, position):
+        new_segment = Turtle(shape="square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())         # get position of the last item in list
 
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
